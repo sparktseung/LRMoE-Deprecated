dbe78eef2d7793a1f6b0f078ac03a9ab74851c2d
sparktseung <sparkingca@gmail.com>
2020-02-29 00:35:56 -0500

    Added ExpertWeibull.R

---
+++ test/test_ExpertWeibull.R
@@ -0,0 +1,15 @@
+# Test the expert function outputs the correct values
+
+N = 100
+
+tl = rep(c(0, 1), 4*N)
+yl = rep(c(1, 2, 3, 4), 2*N)
+yu = rep(c(1, 2, 4, 4), 2*N)
+tu = rep(c(4, 5, Inf, Inf), 2*N)
+g = 1
+shape.k = 2
+scale.lambda = 0.5
+
+temp = expert.weibull(tl, yl, yu, tu, g = 1, shape.k, scale.lambda)
+
+head(cbind(temp[[1]], temp[[2]], temp[[3]]), 8)
