df2ffb163ee6bb963e043fe24895dd086f477c3f
sparktseung <sparkingca@gmail.com>
2020-03-01 21:10:55 -0500

    Added DemoData

---
+++ R/DemoData.R
@@ -0,0 +1,14 @@
+#' Demo data for \code{LRMoE}.
+#'
+#' \itemize{
+#'   \item \code{X}: A matrix of covariates required by the LRMoE fitting function (complete).
+#'   \item \code{Y}: A matrix of response required by the LRMoE fitting function (complete).
+#'   \item \code{X.obs}: A matrix of covariates required by the LRMoE fitting function (after truncation and censoring).
+#'   \item \code{Y.obs}: A matrix of response required by the LRMoE fitting function (after truncation and censoring).
+#' }
+#'
+#' @docType data
+#' @keywords data
+#' @name DemoData
+#'
+NULL
