c68af220cf79ebb9893e00ab0744d8bd54418323
sparktseung <sparkingca@gmail.com>
2020-03-02 21:34:30 -0500

    Tested EMGammaCount.R

--- test/test_GammaCount.R
+++ test/test_GammaCount.R
@@ -19,8 +19,8 @@ comp.dist = matrix(c("gammacount", "ZI-gammacount",
 zero.prob = matrix(c(0, 0.3,
                      0.5, 0),
                    nrow = d, byrow = TRUE)
-params.list = list(list(c(5, 0.2), c(10, 0.5)),
-                   list(c(6, 0.3), c(8, 0.4)))
+params.list = list(list(c(5, 2), c(10, 5)),
+                   list(c(6, 3), c(8, 4)))
 
 hyper.alpha = 5
 hyper.params = list(list(c(2, 1, 2, 1), c(2, 1, 2, 1)),
@@ -38,8 +38,8 @@ comp.dist = comp.dist
 zero.guess = matrix(c(0, 0.8,
                       0.3, 0),
                     nrow = d, byrow = TRUE)
-params.guess = list(list(c(5, 0.5), c(9, 0.4)),
-                    list(c(8, 0.2), c(7, 0.5)))
+params.guess = list(list(c(5, 5), c(9, 4)),
+                    list(c(8, 2), c(7, 5)))
 
 Y.obs = cbind(rep(0, N), Y.gc[,1], Y.gc[,1], rep(Inf, N),
               rep(0, N), Y.gc[,2], Y.gc[,2], rep(Inf, N))
@@ -48,7 +48,7 @@ Y.obs = cbind(rep(0, N), Y.gc[,1], Y.gc[,1], rep(Inf, N),
 model.fit = LRMoE.fit(Y = Y.obs, X = X,
                       n.comp = 2, comp.dist = comp.dist,
                       alpha.init = alpha.guess,
-                      zero.init = zero.guess,
+                      zero.init = zero.guess,s
                       params.init = params.guess,
                       penalty = TRUE, hyper.alpha = hyper.alpha, hyper.params = hyper.params)
 
